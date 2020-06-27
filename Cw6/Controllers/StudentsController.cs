a140b1e35addb2c20a9eae07c8d9401cd9b94c85
Maciej Adrych <MaciejA@bonair.com.pl>
2020-06-27 18:51:53 +0200

    Dodanie StudentsController

---
+++ Cw6/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw6.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>();
+            list.Add(new Student { IdStudent = 1, FirstName = "Jan", LastName="Kowalski" });
+            list.Add(new Student { IdStudent = 2, FirstName = "Andrzej", LastName = "Malewski" });
+
+
+
+            return Ok();
+        }
+    }
+}
