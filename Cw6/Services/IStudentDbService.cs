8c15bd102ae7750d0928d25c32bab699c070f622
Maciej Adrych <MaciejA@bonair.com.pl>
2020-06-27 18:45:22 +0200

    Dodanie folderu service

---
+++ Cw6/Services/IStudentDbService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw6.Services
+{
+    public interface IStudentDbService
+    {
+        IEnumerable<Student> GetStudents();
+    }
+}
---
+++ Cw6/Services/SqlServerStudentDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw6.Services
+{
+    public class SqlServerStudentDbService : IStudentDbService
+    {
+        public IEnumerable<Student> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
